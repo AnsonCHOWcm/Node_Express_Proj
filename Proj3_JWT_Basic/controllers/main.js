f3395f12e04cb6586d6458c0985940720eca0286
AnsonCHOWcm <74531677+AnsonCHOWcm@users.noreply.github.com>
2023-02-02 00:33:27 +0800

    finished creating the JWT and sending to Front End

---
+++ Proj3_JWT_Basic/controllers/main.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const CustomAPIError = require('../errors/custom-error')
+require('dotenv').config();
+
+const login = async (req, res) => {
+    const {username,  password} = req.body
+    console.log({username,  password})
+
+    if (!username || !password){
+        throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+    const id = new Date().getDate()
+    const token = jwt.sign({id, username},process.env.JWT_SERCET, {expiresIn: '30d'})
+
+
+    res.status(200).json({msg: 'user created', token})
+}
+
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: `Hello,`, sercet : `Here is your authoerized data, your lucky number is ${luckyNumber}`})
+
+}
+module.exports = {
+    login,
+    dashboard,
+}
